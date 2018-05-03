7377b2dfb591f0e3f8e80f1fbc8d37db2619c581
Bago <jonas.kris.collier@gmail.com>
2018-05-04 01:59:26 +0200

    Update bitcoin.rb

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -761,7 +761,8 @@ module Bitcoin
       checkpoints: {
              0 => "ff23a3590cc6e308f1817283cfa807793aae41d6a1de9f30ed15b7e8361d7005",
           4700 => "822f46e6c93fafc4aa340b07bf684843346fac32674b2b4920ae271d67f1b722",
-          8200 => "975f7aaf5c0676e457a05bea3e4a2a269b79481411ab9c9aa568a6d5e21a4ce4",          
+          8200 => "975f7aaf5c0676e457a05bea3e4a2a269b79481411ab9c9aa568a6d5e21a4ce4",
+          24916 => "4b5c69fc33b030e1b74e0eb747ba90d4335b56e3cc10ec0c42d3ea230948465b", 
       },
       auxpow_chain_id: 1,
     })
